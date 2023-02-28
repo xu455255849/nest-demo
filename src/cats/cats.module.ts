53c9ce43a4fcff6a1f851421eef12c910d78b671
xushaoping <xusp@yunxi.tv>
2023-02-28 20:00:56 +0800

    update code

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 
+@Global()
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
+  // exports: [CatsService],
 })
 export class CatsModule {}
