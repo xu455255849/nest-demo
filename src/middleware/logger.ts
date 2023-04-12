124906fc24a2b5ee5d390719316c8069313dba94
xushaoping <xusp@yunxi.tv>
2023-04-12 17:02:43 +0800

    update code

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...' + req);
+    console.log('Request...' + req.url);
     next();
   }
 }
